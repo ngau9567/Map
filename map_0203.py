ab6874ae78b3e0ecb013481d653c3a3a2d4c28f3
ngau9567 <70986836+ngau9567@users.noreply.github.com>
2021-02-11 14:23:16 -0500

    Update map_0203.py

--- map_0203.py
+++ map_0203.py
@@ -97,7 +97,7 @@ st.sidebar.subheader("Multiple Filtering")
 ###############
 
 st.markdown('''
-    #### This website offers Multiple Filtering functions for our database of Chinese Digital Collections. The dashboard link is: <a href='https://arcg.is/14qPaG'>f</a> \n
+    #### This website offers Multiple Filtering functions for our database of Chinese Digital Collections. The dashboard link is: https://esrifed-comm.maps.arcgis.com/apps/opsdashboard/index.html#/31c48340b5ab426c8416446f04954471\n
     #### Choose the desired fields for each categories on the left sidebar, click the "Search" button, and we'll get to work! (You can also conduct a singular search by only entering one input.)\n
 
 ''')
