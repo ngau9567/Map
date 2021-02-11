276ce00e1a49a5d942bd0ec7d620c82d918f4dea
ngau9567 <70986836+ngau9567@users.noreply.github.com>
2021-02-10 22:10:32 -0500

    Update map.py

--- map.py
+++ map.py
@@ -28,7 +28,7 @@ st.sidebar.markdown('''
 
 @st.cache
 def load_data():
-    map_df = pd.read_csv("Final_Metadata_Two.csv", encoding='latin1')
+    map_df = pd.read_csv("Final_Metadata_Final.csv", encoding='latin1')
     return map_df
 
 data = load_data()
