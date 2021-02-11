f284b93c130ce6a8224bc3b28960ee8b3eade1dd
ngau9567 <70986836+ngau9567@users.noreply.github.com>
2021-02-10 22:11:53 -0500

    Update map.py

--- map.py
+++ map.py
@@ -74,7 +74,7 @@ st.title("🌍 Collection Exploration on Inputing Year")
 
 @st.cache
 def load_data():
-    map_df = pd.read_csv("Final_Metadata_Two.csv", encoding='latin1')
+    map_df = pd.read_csv("Final_Metadata_Final.csv", encoding='latin1')
     return map_df
 
 data = load_data()
